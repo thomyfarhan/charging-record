dcf63892139f2a506c5b63c04191bd05a5961e95
Thomy <thomyfarhan@gmail.com>
2019-12-07 16:58:28 +0700

    [Fragment Record] Add Fragment

---
+++ app/src/main/java/com/aesthomic/chargingrecord/RecordFragment.kt
@@ -0,0 +1,20 @@
+package com.aesthomic.chargingrecord
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RecordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_record, container, false)
+    }
+
+
+}

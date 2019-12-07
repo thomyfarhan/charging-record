8d2f833f981bf68792b7364b4f6e441088bba65c
Thomy <thomyfarhan@gmail.com>
2019-12-07 19:36:43 +0700

    [Database Record] Make Record Model as Entity

--- app/src/main/java/com/aesthomic/chargingrecord/database/Record.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/database/Record.kt
@@ -1,10 +1,26 @@
 package com.aesthomic.chargingrecord.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "record_table")
 data class Record(
+    @PrimaryKey(autoGenerate = true)
     var id: Long = 0L,
+
+    @ColumnInfo(name = "startTimeMilli")
     val startTimeMilli: Long = 0L,
+
+    @ColumnInfo(name = "endTimeMilli")
     var endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "startBatteryLevel")
     var startBatteryLevel: Int = 0,
+
+    @ColumnInfo(name = "endBatteryLevel")
     var endBatteryLevel: Int = startBatteryLevel,
+
+    @ColumnInfo(name = "phoneHeat")
     var phoneHeat: Int = -1
 )

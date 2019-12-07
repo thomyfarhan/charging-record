c910d427a7f376f02fee3231debe9697dafe45b1
Thomy <thomyfarhan@gmail.com>
2019-12-07 20:46:51 +0700

    [Database] Create companion object inside database

--- app/src/main/java/com/aesthomic/chargingrecord/database/ChargingRecordDatabase.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/database/ChargingRecordDatabase.kt
@@ -1,6 +1,8 @@
 package com.aesthomic.chargingrecord.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 /**
@@ -10,4 +12,61 @@ import androidx.room.RoomDatabase
  * you'll have to increase the version if schema has been changed
  */
 @Database(entities = [Record::class], version = 1, exportSchema = false)
-abstract class ChargingRecordDatabase: RoomDatabase() {}
+abstract class ChargingRecordDatabase: RoomDatabase() {
+
+    /**
+     * Database needs to know the DAO
+     * DAO could be multiple
+     */
+    abstract val recordDao: RecordDao
+
+    companion object {
+
+        /**
+         * This variable will keep a reference to the database
+         * It will helps you avoid repeatedly opening connection to database
+         */
+        @Volatile
+        private var INSTANCE: ChargingRecordDatabase? = null
+
+        /**
+         * Get the Database
+         * if database has already been retrieved
+         * the previous database will be returned
+         * Otherwise create new database
+         */
+        fun getInstance(context: Context): ChargingRecordDatabase {
+
+            /**
+             * Wrapping code into the synchronized
+             * will limit to one thread execution at a time
+             * to enter this block of code
+             */
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    /**
+                     * if instance is null,
+                     * use databaseBuilder to get a database
+                     */
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ChargingRecordDatabase::class.java,
+                        "charging_record_database"
+                    )
+                        /**
+                         * Destroy and rebuild database
+                         * if schema had changed
+                         */
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}

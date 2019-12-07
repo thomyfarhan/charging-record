f4067457f009f140bc9fe913a6aed232bc3dea1c
Thomy <thomyfarhan@gmail.com>
2019-12-07 23:22:25 +0700

    [Fragment Record] Create functions to initialize current record

--- app/src/main/java/com/aesthomic/chargingrecord/record/RecordViewModel.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/record/RecordViewModel.kt
@@ -2,7 +2,10 @@ package com.aesthomic.chargingrecord.record
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.aesthomic.chargingrecord.database.Record
 import com.aesthomic.chargingrecord.database.RecordDao
+import kotlinx.coroutines.*
 
 /**
  * AndroidViewModel is the same class as ViewModel
@@ -13,4 +16,50 @@ class RecordViewModel(
     val database: RecordDao,
     application: Application) : AndroidViewModel(application) {
 
+    /**
+     * This variable allows to cancel all coroutines started by this view model
+     * When view model is no longer used or destroyed
+     */
+    private val viewModelJob = Job()
+
+    /**
+     * This scope determines which thread this coroutine will run on
+     * uiScope will run on Main thread (Dispatcher.Main)
+     */
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var currentRecord = MutableLiveData<Record>()
+
+    init {
+        initializeCurrentRecord()
+    }
+
+    private fun initializeCurrentRecord() {
+        uiScope.launch {
+            currentRecord.value = getRecordFromDatabase()
+        }
+    }
+
+    /**
+     * suspend function to do a running work
+     * so the UI thread won't get blocked while waiting for result
+     */
+    private suspend fun getRecordFromDatabase(): Record? {
+        /**
+         * Use Dispatcher.IO as thread
+         * because getting data from database is I/O Operation
+         */
+        return withContext(Dispatchers.IO) {
+            var record = database.getNewest()
+            if (record?.startTimeMilli != record?.endTimeMilli) {
+                record = null
+            }
+            record
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }

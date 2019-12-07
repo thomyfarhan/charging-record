a4cb8097acbcdc1818a02cc574077e9404a4d406
Thomy <thomyfarhan@gmail.com>
2019-12-07 20:09:59 +0700

    [Database Record] Add functions in DAO

--- app/src/main/java/com/aesthomic/chargingrecord/database/RecordDao.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/database/RecordDao.kt
@@ -1,6 +1,61 @@
 package com.aesthomic.chargingrecord.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
+/**
+ * Interface with some functions for accessing database with query
+ */
 @Dao
-interface RecordDao {}
+interface RecordDao {
+
+    /**
+     * Create new row to the database
+     *
+     * @param record new value to create
+     */
+    @Insert
+    fun insert(record: Record)
+
+    /**
+     * When a row with same id already existed
+     * the old row will replaced with the new one
+     *
+     * @param record new value to replace the old one
+     */
+    @Update
+    fun update(record: Record)
+
+    /**
+     * Fetch record value that matches with id which is key param
+     *
+     * @param key value to find match row
+     */
+    @Query("SELECT * FROM record_table WHERE id = :key")
+    fun get(key: Long): Record?
+
+    /**
+     * Fetch all rows ordered by the newest row
+     * And return the list with form of LiveData
+     */
+    @Query("SELECT * FROM record_table ORDER BY id DESC")
+    fun getAllRecords(): LiveData<List<Record>>
+
+    /**
+     * Fetch row with the highest id
+     * which is the newest one that has been added
+     */
+    @Query("SELECT * FROM record_table ORDER BY id DESC LIMIT 1")
+    fun getNewest(): Record?
+
+    /**
+     * Delete all rows from database
+     * Table will not be deleted, only the rows
+     */
+    @Query("DELETE FROM record_table")
+    fun clear()
+
+}

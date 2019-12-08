7d4176a3ed16a042269d3756480080e45454d1e4
Thomy <thomyfarhan@gmail.com>
2019-12-08 18:41:03 +0700

    [Fragment Phone Heat] Define Job and uiScope

--- app/src/main/java/com/aesthomic/chargingrecord/phoneheat/PhoneHeatViewModel.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/phoneheat/PhoneHeatViewModel.kt
@@ -2,7 +2,19 @@ package com.aesthomic.chargingrecord.phoneheat
 
 import androidx.lifecycle.ViewModel
 import com.aesthomic.chargingrecord.database.RecordDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class PhoneHeatViewModel(
     private val recordId: Long,
-    private val database: RecordDao) : ViewModel() {}
+    private val database: RecordDao) : ViewModel() {
+
+    val viewModelJob = Job()
+    val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}

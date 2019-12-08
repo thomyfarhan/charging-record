826f1bc671f807551b811f494eb964ef98c81935
Thomy <thomyfarhan@gmail.com>
2019-12-08 14:34:22 +0700

    [Fragment Phone Heat] Change layout and implement binding

--- app/src/main/java/com/aesthomic/chargingrecord/phoneheat/PhoneHeatFragment.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/phoneheat/PhoneHeatFragment.kt
@@ -6,16 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.chargingrecord.R
+import com.aesthomic.chargingrecord.databinding.FragmentPhoneHeatBinding
 
 class PhoneHeatFragment : Fragment() {
+
+    private lateinit var binding: FragmentPhoneHeatBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_phone_heat, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_phone_heat, container, false)
+        return binding.root
     }
 
 

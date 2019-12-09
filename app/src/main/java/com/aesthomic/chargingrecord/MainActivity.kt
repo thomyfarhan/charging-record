1ae73b32d428d5c2bb027d440d6e0c8f28e6e7ee
Thomy <thomyfarhan@gmail.com>
2019-12-09 22:57:23 +0700

    [Activity Main] Setup Bottom Navigation

--- app/src/main/java/com/aesthomic/chargingrecord/MainActivity.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/MainActivity.kt
@@ -3,6 +3,9 @@ package com.aesthomic.chargingrecord
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.aesthomic.chargingrecord.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment_main)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        NavigationUI.setupWithNavController(binding.navBottomMain, navController)
     }
 }

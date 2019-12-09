a24e3cb67c6ab49a7e7de69a64830e8926c0861d
Thomy <thomyfarhan@gmail.com>
2019-12-09 23:15:11 +0700

    [Fragment Status] Add View Model

---
+++ app/src/main/java/com/aesthomic/chargingrecord/status/StatusViewModel.kt
@@ -0,0 +1,7 @@
+package com.aesthomic.chargingrecord.status
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class StatusViewModel(
+    application: Application): AndroidViewModel(application) {}

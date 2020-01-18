4cb67a8d6eb265a781d26581d37dfbcb94e810d6
Toms <thomyfarhan@gmail.com>
2020-01-18 17:44:33 +0700

    [Activity Main] Add Up Button

--- app/src/main/java/com/aesthomic/chargingrecord/MainActivity.kt
+++ app/src/main/java/com/aesthomic/chargingrecord/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -12,12 +13,13 @@ import com.aesthomic.chargingrecord.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val navController = this.findNavController(R.id.nav_host_fragment_main)
+        navController = this.findNavController(R.id.nav_host_fragment_main)
         val appBarConfiguration = AppBarConfiguration
             .Builder(
                 R.id.record_destination,
@@ -38,5 +40,9 @@ class MainActivity : AppCompatActivity() {
                 binding.navBottomMain.visibility = View.GONE
             }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
